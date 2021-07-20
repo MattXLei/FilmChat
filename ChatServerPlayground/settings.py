57bc4a56e6d55e57d656c2a58beec230b1a685df
Matthew_Lei <thepetscompany123@gmail.com>
2021-07-20 12:05:38 -0400

    local db setting

--- ChatServerPlayground/settings.py
+++ ChatServerPlayground/settings.py
@@ -89,22 +89,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'ChatServerPlayground.wsgi.application'
 ASGI_APPLICATION = "ChatServerPlayground.routing.application"
 
-# Database
-# https://docs.djangoproject.com/en/2.2/ref/settings/#databases
-
-DB_NAME = "film_chat_dev"
-DB_USER = "django"
-DB_PASSWORD = "pass123"
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': DB_NAME,
-        'USER': DB_USER,
-        'PASSWORD': DB_PASSWORD,
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
 
 CHANNEL_LAYERS = {
     'default': {
@@ -167,3 +151,9 @@ BASE_URL = "http://127.0.0.1:8000"
 
 
 DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760  # 10 MB
+
+
+try:
+    from .local_db import *
+except ImportError:
+    pass
