d8824a2e5b1d8f535b63c7a1e45f2bee3cdd78d8
Matthew_Lei <thepetscompany123@gmail.com>
2021-08-30 13:32:30 -0400

    Add collections.js, Real-time update for notifications

--- notification/constants.py
+++ notification/constants.py
@@ -1,4 +1,4 @@
-DEFAULT_NOTIFICATION_PAGE_SIZE = 3
+DEFAULT_NOTIFICATION_PAGE_SIZE = 10
 
 
 """
@@ -10,5 +10,7 @@ DEFAULT_NOTIFICATION_PAGE_SIZE = 3
 GENERAL_MSG_TYPE_NOTIFICATIONS_PAYLOAD = 0
 # No more 'general' notifications to retrieve
 GENERAL_MSG_TYPE_PAGINATION_EXHAUSTED = 1
+# Retrieved all 'general' notifications newer than the oldest visible on screen
+GENERAL_MSG_TYPE_NOTIFICATIONS_REFRESH_PAYLOAD = 2
 # Update a notification that has been altered (Ex: Accept/decline a friend request)
 GENERAL_MSG_TYPE_UPDATED_NOTIFICATION = 5
